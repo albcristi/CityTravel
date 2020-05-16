29c76c067bd0ca192acc9b92e5e3e78b1533bf52
Alb Dan Cristian <alb.cristian_dan@yahoo.com>
2020-05-16 18:12:37 +0300

    Breaaak

--- src/main/java/model/UserRoute.java
+++ src/main/java/model/UserRoute.java
@@ -1,6 +1,7 @@
 package model;
 
 import lombok.*;
+import org.json.simple.JSONObject;
 
 
 @Getter
@@ -14,4 +15,14 @@ public class UserRoute {
     private Integer user_id;
     private Integer city_id;
     private String city_name;
+
+
+    public JSONObject toJsonFormat(){
+        JSONObject object = new JSONObject();
+        object.put("route_id",id);
+        object.put("user_id", user_id);
+        object.put("city_id",city_id);
+        object.put("city_name", city_name);
+        return object;
+    }
 }
--- src/main/java/servlets/UserRouteServlet.java
+++ src/main/java/servlets/UserRouteServlet.java
@@ -1,6 +1,56 @@
 package servlets;
 
+import model.User;
+import model.UserRoute;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import service.AuthenticationService;
+import service.UserRouteService;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
 
 public class UserRouteServlet extends HttpServlet {
+    private UserRouteService userRouteService = new UserRouteService();
+
+    private AuthenticationService authenticationService = new AuthenticationService();
+
+    private RequestHelper requestHelper = new RequestHelper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Optional<User> sessionUser = authenticationService.validateUserRequest(req);
+        if(sessionUser.isPresent()){
+            //yay..
+            JSONArray jsonArray = new JSONArray();
+            userRouteService.getUserRoutes(sessionUser.get().getId())
+                    .stream()
+                    .map(UserRoute::toJsonFormat)
+                    .forEach(jsonArray::add);
+            requestHelper.writeBody(resp, HttpServletResponse.SC_OK,jsonArray.toJSONString());
+        }
+        else {
+            requestHelper.writeUnauthorizedResponse(resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Optional<User> sessionUser = authenticationService.validateUserRequest(req);
+        if(sessionUser.isPresent()){
+            //yay..
+            JSONObject reqBody = requestHelper.readRequestBody(req);
+            Integer city_id = Integer.parseInt( (String) reqBody.get("city_id"));
+            Boolean res = userRouteService.saveRoute(sessionUser.get().getId(), city_id);
+            requestHelper.writeBody(resp, HttpServletResponse.SC_OK,res.toString());
+
+        }
+        else {
+            requestHelper.writeUnauthorizedResponse(resp);
+        }
+    }
 }
